ff73b5c7e7e46efe4d0abdbcaba0063c409ae99b
Reza Esfandiari <resfandiari.dev@gmail.com>
2020-02-11 11:25:27 +0330

    Add files via upload

---
+++ android/app/src/main/kotlin/io/github/resfandiari/flutter_firebase_messaging/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.resfandiari.flutter_firebase_messaging
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
